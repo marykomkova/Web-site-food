b183eb5286ef096fb755adbc57b7e018cb7db479
Mary <marykomkova@gmail.com>
2021-10-18 01:02:11 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/* jshint -W097 */
+/* jshint -W104 */
+/* jshint -W117 */
+'use strict';
+
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/js'
+  },
+  watch: true,
+
+  devtool: "source-map",
+
+  module: {}
+};
